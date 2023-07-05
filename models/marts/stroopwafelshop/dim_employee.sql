c5ac5d1637b76e3ad610ff32ea3baee13ee48722
Lasse Benninga <devops.pipeline@example.com>
2023-07-05 14:05:57 +0200

    update

--- models/marts/stroopwafelshop/dim_employee.sql
+++ models/marts/stroopwafelshop/dim_employee.sql
@@ -1,1 +1,23 @@
-with stg_employee as {{ ref()}}
+with stg_employee as (
+
+    select *
+    from {{ ref('stg_employees') }}
+
+),
+
+final as (
+
+    select
+        employee_id,
+        employee_name,
+        employee_last_name,
+        employee_contact,
+        employee_date_of_birth,
+        employee_since,
+        position
+
+    from stg_employee
+
+)
+
+select * from final
