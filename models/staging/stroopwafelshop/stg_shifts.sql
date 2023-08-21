5b6f28a6062353f1141ff2a16cc3610aa3d6dc5f
Lasse Benninga <devops.pipeline@example.com>
2023-08-21 14:17:25 +0200

    split shift hours

--- models/staging/stroopwafelshop/stg_shifts.sql
+++ models/staging/stroopwafelshop/stg_shifts.sql
@@ -11,13 +11,12 @@ with
             cast(position as string) as position,
             cast(employee_name as string) as employee_name,
             cast(employee_id as float64) as employee_id,
-            cast(shift_hours as string) as shift_hours
             safe_cast(
-                split(split(shift_time, '-')[offset(0)], ':')[offset(0)] as int64
-            ) as shift_start_hour,
+                split(split(shift_hours, '-')[offset(0)], ':')[offset(0)] as int64
+            ) as start_hour,
             safe_cast(
-                split(split(shift_time, '-')[offset(1)], ':')[offset(0)] as int64
-            ) as shift_end_hour
+                split(split(shift_hours, '-')[offset(1)], ':')[offset(0)] as int64
+            ) as end_hour
 
         from raw_source
 
